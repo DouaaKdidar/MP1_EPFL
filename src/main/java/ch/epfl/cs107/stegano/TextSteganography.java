05dcdf1dca3dd5ff0b66b548dc23fb7649ab7d7a
Douaa Kdidar <douaa.kdidar@epfl.ch>
2025-10-17 19:36:41 +0100

    textSteganography

--- src/main/java/ch/epfl/cs107/stegano/TextSteganography.java
+++ src/main/java/ch/epfl/cs107/stegano/TextSteganography.java
@@ -1,6 +1,9 @@
 package ch.epfl.cs107.stegano;
 
 import ch.epfl.cs107.Helper;
+import ch.epfl.cs107.crypto.Encrypt;
+import ch.epfl.cs107.utils.Bit;
+import ch.epfl.cs107.utils.Text;
 
 import static ch.epfl.cs107.utils.Text.*;
 import static ch.epfl.cs107.utils.Image.*;
@@ -20,6 +23,7 @@ import static ch.epfl.cs107.Main.*;
  */
 public class TextSteganography {
 
+
     // DO NOT CHANGE THIS, MORE ON THAT ON WEEK 7
     private TextSteganography(){}
 
@@ -34,7 +38,23 @@ public class TextSteganography {
      * @return ARGB image with the message embedded
      */
     public static int[][] embedBitArray(int[][] cover, boolean[] message) {
-        return Helper.fail("NOT IMPLEMENTED");
+        assert cover != null ;
+        assert message != null ;
+        int n = 0 ;
+        int m = 0 ;
+        int max = cover[0].length ;
+        int MAX = cover.length ;
+        for(int i = 0 ; i < message.length ; ++i ){
+            cover[n][m] =Bit.embedInLSB(cover[n][m] , message[i]) ;
+            if(m == max-1){
+                if(n == MAX-1) return cover ;
+                m = 0 ;
+                ++n ;
+            }else{
+                ++m ;
+            }
+        }
+        return cover ;
     }
 
     /**
@@ -43,7 +63,16 @@ public class TextSteganography {
      * @return extracted message
      */
     public static boolean[] revealBitArray(int[][] image) {
-        return Helper.fail("NOT IMPLEMENTED");
+        assert image != null ;
+        int n = image.length ;
+        int m = image[0].length ;
+        boolean bitArray[] = new boolean[n*m] ;
+        for(int i = 0; i  < n ; ++i){
+            for(int j = 0 ; j  < m ; ++j){
+                bitArray[m*i+j] =  Bit.getLSB(image[i][j]) ;
+            }
+        }
+        return bitArray;
     }
 
 
@@ -59,7 +88,10 @@ public class TextSteganography {
      * @return ARGB image with the message embedded
      */
     public static int[][] embedText(int[][] cover, byte[] message) {
-        return Helper.fail("NOT IMPLEMENTED");
+        assert cover != null ;
+        assert message != null ;
+        boolean[] bitArray = Text.toBitArray(Text.toString(message)) ;
+        return embedBitArray(cover , bitArray) ;
     }
 
     /**
@@ -68,7 +100,35 @@ public class TextSteganography {
      * @return extracted message
      */
     public static byte[] revealText(int[][] image) {
-        return Helper.fail("NOT IMPLEMENTED");
+        assert image != null ;
+        boolean[] bitArray = revealBitArray(image) ;
+        String text = Text.toString(bitArray) ;
+        return Text.toBytes(text) ;
+    }
+
+
+
+
+
+
+
+
+    // section 5.4
+    public static int[][] embedText(int[][] cover , byte[] message , String encryptionMethodName , byte[] key){
+        byte[] hidden = new byte[message.length] ;
+         switch(encryptionMethodName){
+             case "cbc" :
+                 hidden = Encrypt.cbc(message , key) ;
+                 break ;
+
+
+         }
+         cover = embedText(cover , hidden) ;
+         return cover ;
+    }
+
+    public static void revealText(int[][] image , String encryptionMethodName , byte[] key){
+
     }
 
 }
